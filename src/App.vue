131b3c46a8e24ac058493c5775eb2322f0177573
whbqd <1808602879@qq.com>
2020-04-19 09:46:57 +0800

    登录页面
    Signed-off-by: whbqd <51955400+whbqd@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,14 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <login />
   </div>
 </template>
+<script>
+    import login from "./components/login";
 
-<style lang="scss" scoped>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-  }
-  a.router-link-exact-active {
-    color: #42b983;
-  }
-}
-</style>
+    export default {
+        components: {
+            login
+        }
+    };
+</script>
