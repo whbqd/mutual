065840c8ff3f31a15acc8cbca0cb42c876b16bd7
whbqd <51955400+whbqd@users.noreply.github.com>
2020-05-09 09:59:31 +0800

    登录功能前后端分离

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,36 @@
 <template>
   <div id="app">
-    <router-view />
+    <router-view/>
+    <div class="record">
+      <span
+      ><a href="http://www.beian.miit.gov.cn/">浙ICP备20015412号-1</a></span
+      >
+    </div>
     <!--<register />-->
   </div>
 </template>
 <script>
-export default {};
+    export default {};
 </script>
+<style lang="scss">
+  .record {
+    width: 400px;
+    position: absolute;
+    bottom: 1%;
+    left: 50%;
+    transform: translateX(-50%);
+    text-align: center;
+
+    span {
+      a {
+        color: #000;
+
+        &:hover {
+          color: #2c3e50;
+        }
+      }
+    }
+  }
+
+
+</style>
